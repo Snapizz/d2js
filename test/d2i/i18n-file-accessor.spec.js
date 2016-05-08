9c7a55f70fd12168cb55632134bd73321b30ab98
snapiz97 <jonathan.lapiquonne@gmail.com>
2016-05-08 17:59:16 +0200

    add test and optimize d2i reader

--- test/d2i/i18n-file-accessor.spec.js
+++ test/d2i/i18n-file-accessor.spec.js
@@ -2,55 +2,51 @@ import I18nFileAccessor from '../../lib/d2i/i18n-file-accessor';
 
 describe('I18nFileAccessor', function () {
   var filename = 'test/fixtures/d2i/i18n_fr.d2i';
-  var d2i = new I18nFileAccessor(filename);
-  d2i.init();
-  it('should have filename!', function () {
-    expect(d2i.filename).toEqual(filename);
-  });
+  I18nFileAccessor.init(filename);
   it('should have _stream!', function () {
-    expect(d2i._stream).toBeDefined();
+    expect(I18nFileAccessor._stream).toBeDefined();
   });
   it('should have _unDiacriticalIndex!', function () {
-    expect(d2i._unDiacriticalIndex).toBeDefined();
+    expect(I18nFileAccessor._unDiacriticalIndex).toBeDefined();
   });
   it('should have _indexes!', function () {
-    expect(d2i._indexes).toBeDefined();
+    expect(I18nFileAccessor._indexes).toBeDefined();
   });
   it('should have _textIndexes!', function () {
-    expect(d2i._textIndexes).toBeDefined();
+    expect(I18nFileAccessor._textIndexes).toBeDefined();
   });
   it('should have _textIndexesOverride!', function () {
-    expect(d2i._textIndexesOverride).toBeDefined();
+    expect(I18nFileAccessor._textIndexesOverride).toBeDefined();
   });
   it('should have _textSortIndex!', function () {
-    expect(d2i._textSortIndex).toBeDefined();
+    expect(I18nFileAccessor._textSortIndex).toBeDefined();
   });
   it('should have _textCount!', function () {
-    expect(d2i._textCount).toBeDefined();
-    expect(d2i._textCount).toBeGreaterThan(0);
+    expect(I18nFileAccessor._textCount).toBeDefined();
+    expect(I18nFileAccessor._textCount).toBeGreaterThan(0);
   });
   it('should override id!', function () {
-    expect(d2i.overrideId).toBeDefined();
+    expect(I18nFileAccessor.overrideId).toBeDefined();
   });
   it('should get order index!', function () {
-    expect(d2i.getOrderIndex).toBeDefined();
+    expect(I18nFileAccessor.getOrderIndex).toBeDefined();
   });
   it('should get text!', function () {
-    expect(d2i.getText).toBeDefined();
+    expect(I18nFileAccessor.getText).toBeDefined();
   });
   it('should get undiacritical text!', function () {
-    expect(d2i.getUnDiacriticalText).toBeDefined();
+    expect(I18nFileAccessor.getUnDiacriticalText).toBeDefined();
   });
   it('should verify if text exists!', function () {
-    expect(d2i.hasText).toBeDefined();
+    expect(I18nFileAccessor.hasText).toBeDefined();
   });
   it('should get named text!', function () {
-    expect(d2i.getNamedText).toBeDefined();
+    expect(I18nFileAccessor.getNamedText).toBeDefined();
   });
   it('should verify if named text exists!', function () {
-    expect(d2i.hasNamedText).toBeDefined();
+    expect(I18nFileAccessor.hasNamedText).toBeDefined();
   });
   it('should use direct buffer!', function () {
-    expect(d2i.useDirectBuffer).toBeDefined();
+    expect(I18nFileAccessor.useDirectBuffer).toBeDefined();
   });
 });
--- test/d2p/game-data-file-accessor.spec.js
+++ test/d2p/game-data-file-accessor.spec.js
@@ -3,8 +3,18 @@ import GameDataFileAccessor from '../../lib/d2p/game-data-file-accessor';
 describe('GameDataFileAccessor', function () {
   var filename = 'test/fixtures/d2p/Mounts.d2o';
   GameDataFileAccessor.init(filename);
-  console.log(GameDataFileAccessor.getObjects());
   it('should have objects!', function () {
     expect(GameDataFileAccessor.counter).toBeGreaterThan(0);
+  });
+  it('should get object!', function () {
+    var mount = GameDataFileAccessor.getObject(41);
+    expect(mount).toBeDefined();
+    expect(mount.nameId).toEqual(59093);
+  });
+  it('should get objects!', function () {
+    var mounts = GameDataFileAccessor.getObjects();
+    expect(mounts.length).toBeGreaterThan(0);
+    expect(mounts[24].id).toEqual(41);
+    expect(mounts[24].nameId).toEqual(59093);
   });
 });
