27b8e8ad69b5e81fa9202e02fb69fa3aa4f3e879
snapiz97 <jonathan.lapiquonne@gmail.com>
2016-05-08 18:39:10 +0200

    add map for game data accessor

--- test/d2p/game-data-file-accessor.spec.js
+++ test/d2p/game-data-file-accessor.spec.js
@@ -1,4 +1,5 @@
 import GameDataFileAccessor from '../../lib/d2p/game-data-file-accessor';
+import I18nFileAccessor from '../../lib/d2i/i18n-file-accessor';
 
 describe('GameDataFileAccessor', function () {
   var filename = 'test/fixtures/d2p/Mounts.d2o';
@@ -16,5 +17,9 @@ describe('GameDataFileAccessor', function () {
     expect(mounts.length).toEqual(25);
     expect(mounts[24].id).toEqual(41);
     expect(mounts[24].nameId).toEqual(59093);
+  });
+  it('should map!', function () {
+    var mounts = GameDataFileAccessor.map((e) => I18nFileAccessor.getText(e.nameId));
+    expect(mounts[24]).toEqual('Dragodinde Amande et Pourpre');
   });
 });
