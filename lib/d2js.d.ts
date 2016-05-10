bcab0dc832e140ce5818914777ba639f43cc3f8a
snapiz97 <jonathan.lapiquonne@gmail.com>
2016-05-10 22:55:49 +0200

    update d.ts

--- lib/d2js.d.ts
+++ lib/d2js.d.ts
@@ -5,23 +5,23 @@ declare module "d2js" {
 declare module "d2js/data" {
   interface GameDataFileAccessor {
     counter: number;
-    init(filename: string): void;
-    getClassDefinition(idx: number): any;
-    getObject(idx: number): any;
-    getObjects(f?: (e: any) => boolean, limit?: number, m?: (e: any) => any): Array<any>;
-    map(m?: (e: any) => Array<any>): Array<any>;
+    register(...entries: {key: string, path: string}[]): void;
+    getClassDefinition(key: string, idx: number): any;
+    getObject(key: string, idx: number): any;
+    getObjects(key: string, f?: (e: any) => boolean, limit?: number, m?: (e: any) => any): Array<any>;
+    map(key: string, m?: (e: any) => Array<any>): Array<any>;
   }
   interface I18nFileAccessor {
-    init(filename: string): void;
-    overrideId(idx1: number, idx2: number): void;
-    getOrderIndex(idx: number): number;
-    getText(idx: number): string;
-    getUnDiacriticalText(idx: number): string;
-    hasText(idx: number): boolean;
-    getNamedText(key: string): string;
-    hasNamedText(key: string): boolean;
-    useDirectBuffer(value: boolean): void;
-    getTexts(f?: (e: any) => boolean, limit?: number);
+    register(...entries: {key: string, path: string}[]): void;
+    overrideId(key: string, idx1: number, idx2: number): void;
+    getOrderIndex(key: string, idx: number): number;
+    getText(key: string, idx: number): string;
+    getUnDiacriticalText(key: string, idx: number): string;
+    hasText(key: string, idx: number): boolean;
+    getNamedText(key: string, name: string): string;
+    hasNamedText(key: string, name: string): boolean;
+    useDirectBuffer(key: string, value: boolean): void;
+    getTexts(key: string, f?: (e: any) => boolean, limit?: number);
   }
   export const D2P: GameDataFileAccessor;
   export const D2I: I18nFileAccessor;
